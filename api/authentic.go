1843b4eff7f0872f177b402cf41986b2e065b218
Niko-Sy <864086367@qq.com>
2025-11-28 17:01:15 +0800

    v1.0

--- api/authentic.go
+++ api/authentic.go
@@ -90,6 +90,31 @@ func HandleRegister(c *gin.Context) error {
 
 }
 
+func HandleGetUserInfo(c *gin.Context) {
+	var getUserInfoReq struct {
+		UserId string `json:"userId" binding:"required"`
+	}
+	if err := c.ShouldBindJSON(&getUserInfoReq); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "无效的请求数据"})
+		return
+	}
+
+	// 模拟数据库查询
+
+	UserInfo := models.UserInfo{
+		UserId:           "U123456789",
+		Username:         "admin",
+		Nickname:         "管理员",
+		Avatar:           "https://example.com/avatar.jpg",
+		Email:            "admin@example.com",
+		Phone:            "111111111111",
+		Signature:        "I am a super admin",
+		OnlineStatus:     "online",
+		GlobalMuteStatus: "unmuted",
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "获取用户信息成功", "data": UserInfo})
+}
 func HandleRefresh(c *gin.Context) {
 
 }
--- models/user.go
+++ models/user.go
@@ -20,3 +20,15 @@ type User struct {
 	RegisterTime      time.Time `json:"registerTime"`                // 注册时间
 	LastLoginTime     time.Time `json:"lastLoginTime"`               // 最后登录时间
 }
+
+type UserInfo struct {
+	UserId           string `json:"userId"`                     // 用户ID（U+9位数字）
+	Username         string `json:"username"`                   // 用户名（唯一）
+	Nickname         string `json:"nickname,omitempty"`         // 昵称
+	Avatar           string `json:"avatar"`                     // 头像URL
+	Email            string `json:"email,omitempty"`            // 邮箱
+	Phone            string `json:"phone,omitempty"`            // 手机号
+	Signature        string `json:"signature,omitempty"`        // 个性签名
+	OnlineStatus     string `json:"onlineStatus"`               // 在线状态: 'online' | 'away' | 'busy' | 'offline'
+	GlobalMuteStatus string `json:"globalMuteStatus,omitempty"` // 全局禁言状态: 'muted' | 'unmuted'
+}
